b9d1c9ceb3a226f6892790ce288f9ef647ecdc5d
Matheus <matheus_guesser@estudante.sesisenai.org.br>
2026-08-19 11:37:36 -0300

    add- php cadastro_prato

--- public/cadastrar_prato.php
+++ public/cadastrar_prato.php
@@ -13,7 +13,32 @@ if ($resultado === false) {
 }
 
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nome = ($_POST['nome']);
+    $descricao = ($_POST['descricao']);
+    $preco = $_POST['preco'];
+    $categoria = ($_POST['categoria']);
+    $usuario_id = $_POST['usuario_id'];
 
+    $sql = "INSERT INTO pratos (nome, descricao, preco, categoria, usuario_id) VALUES (?, ?, ?, ?, ?)";
+    $stmt = mysqli_prepare($conexao, $sql);
+
+    if ($stmt === false) {
+        die("Erro ao preparar consulta: " . mysqli_error($conexao));
+    }
+
+    mysqli_stmt_bind_param($stmt, 'ssdsi', $nome, $descricao, $preco, $categoria, $usuario_id);
+
+    if (mysqli_stmt_execute($stmt)) {
+        echo "Prato cadastrado!";
+        echo "<br><a href='../index.php'>Voltar</a>";
+        exit();
+    } else {
+        echo "Erro ao cadastrar prato: " . mysqli_error($conexao);
+    }
+
+    mysqli_stmt_close($stmt);
+}
 
 
 
