73d0d4a8dc8ddd4a4f66826c4ceacd326419c8c6
Matheus <matheus_guesser@estudante.sesisenai.org.br>
2026-08-19 11:28:33 -0300

    add- remov atualizar

--- database.sql/db.sql
+++ database.sql/db.sql
@@ -1,15 +1,16 @@
 CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(150) NOT NULL
 );
 
 CREATE TABLE pratos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT ,
     nome VARCHAR(100) NOT NULL,
     descricao TEXT NOT NULL,
     preco DECIMAL(10,2) NOT NULL,
     categoria VARCHAR(100) NOT NULL,
+
     usuario_id INT NOT NULL,
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
--- public/cadastrar_prato.php
+++ public/cadastrar_prato.php
@@ -1,3 +1,57 @@
-<!-- Cadastro Usuários -->
+<?php
 
-<!-- Cadastro Pratos -->
+include '../infra/conexao.php';
+if(!isset($conexao) || $conexao === false) {
+    die("Erro: Conexão com o banco de dados não estabelecida.");
+}
+
+$sql = "SELECT * FROM usuarios";
+$resultado = $conexao->query($sql);
+
+if ($resultado === false) {
+    die("Erro na consulta: " . mysqli_error($conexao));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
