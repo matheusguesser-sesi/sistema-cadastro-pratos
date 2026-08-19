d803e623c075a3b5e23d2f7aa1695e6dd31c5d9d
Matheus <matheus_guesser@estudante.sesisenai.org.br>
2026-08-19 11:03:42 -0300

    add- tabelas banco

---
+++ database.sql/db.sql
@@ -0,0 +1,15 @@
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL
+);
+
+CREATE TABLE pratos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    categoria VARCHAR(100) NOT NULL,
+    usuario_id INT NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
