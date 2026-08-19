7654b76cf80103fb89e4b0b8f1e326b214721eb9
Matheus <matheus_guesser@estudante.sesisenai.org.br>
2026-08-19 11:50:54 -0300

    add- atualizacao cadastr_prato

--- public/cadastrar_prato.php
+++ public/cadastrar_prato.php
@@ -5,8 +5,6 @@ if(!isset($conexao) || $conexao === false) {
     die("Erro: Conexão com o banco de dados não estabelecida.");
 }
 
-$sql = "SELECT * FROM usuarios";
-$resultado = $conexao->query($sql);
 
 if ($resultado === false) {
     die("Erro na consulta: " . mysqli_error($conexao));
