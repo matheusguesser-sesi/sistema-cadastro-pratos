a0e3e00c3d1c0264aa758d5163f401ad1915bfc1
caiox <caio_marques-santos@estudante.sesisenai.org.br>
2026-08-19 11:49:23 -0300

    add - commit arrumando index

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro Usuário</title>
+    <link rel="stylesheet" href="style/styles.css">
+</head>
+<body>
+    <main>
+        <h1>Gerenciar PRatos</h1>
+        <a href="public/cadastrar_prato.php">Cadastrar Prato</a>
+        <a href="public/cadastrar_usuario.php">Cadastrar Usuário</a>
+        <br><br>
+        
+</body>
+</html>
+
 <?php
 
 include "infra/conexao.php";
